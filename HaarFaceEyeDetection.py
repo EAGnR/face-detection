40974cbc5bb62dadbb148f8095892a814b02bc98
EAGnR <edwin_gnr@hotmail,.com>
2019-04-21 02:30:01 -0400

    initial commit.

---
+++ HaarFaceEyeDetection.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+face_cascade2 = cv2.CascadeClassifier('OpenCV/haarcascades/haarcascade_frontalface_alt.xml')
+face_cascade3 = cv2.CascadeClassifier('OpenCV/haarcascades/haarcascade_frontalface_alt2.xml')
+face_cascade4 = cv2.CascadeClassifier('OpenCV/haarcascades/haarcascade_frontalface_alt_tree.xml')
+eye_cascade = cv2.CascadeClassifier('OpenCV/haarcascades/haarcascade_eye.xml')
+eye_cascade2 = cv2.CascadeClassifier('OpenCV/haarcascades/haarcascade_lefteye_2splits.xml')
+eye_cascade3 = cv2.CascadeClassifier('OpenCV/haarcascades/haarcascade_righteye_2splits.xml')
+eye_cascade4 = cv2.CascadeClassifier('OpenCV/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+
+img = cv2.imread('images/many-faces.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = list(face_cascade2.detectMultiScale(gray, 1.3, 5))
+for (x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+    
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = list(eye_cascade.detectMultiScale(roi_gray))
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,0,255), 2)
+
+cv2.imshow('img',img)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    vid_faces = list(face_cascade2.detectMultiScale(gray_frame, 1.3, 5))
+    for (x,y,w,h) in vid_faces:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+        
+        roi_gray = gray_frame[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = list(eye_cascade.detectMultiScale(roi_gray))
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,0,255), 2)
+
+    cv2.imshow('frame', frame)
+
+    k = cv2.waitKey(5) & 0xFF
+
+    if k == 27:
+        break
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ OpenCV/OpenCVModules-relwithdebinfo.cmake
@@ -0,0 +1,179 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RelWithDebInfo".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "opencv_core" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_core PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_core.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_core.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_core )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_core "${_IMPORT_PREFIX}/lib64/libopencv_core.so.3.4.1" )
+
+# Import target "opencv_flann" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_flann APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_flann PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_flann.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_flann.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_flann )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_flann "${_IMPORT_PREFIX}/lib64/libopencv_flann.so.3.4.1" )
+
+# Import target "opencv_imgproc" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_imgproc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_imgproc PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_imgproc.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_imgproc.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_imgproc )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_imgproc "${_IMPORT_PREFIX}/lib64/libopencv_imgproc.so.3.4.1" )
+
+# Import target "opencv_ml" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_ml APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_ml PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_ml.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_ml.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_ml )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_ml "${_IMPORT_PREFIX}/lib64/libopencv_ml.so.3.4.1" )
+
+# Import target "opencv_objdetect" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_objdetect APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_objdetect PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_objdetect.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_objdetect.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_objdetect )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_objdetect "${_IMPORT_PREFIX}/lib64/libopencv_objdetect.so.3.4.1" )
+
+# Import target "opencv_photo" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_photo APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_photo PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_photo.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_photo.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_photo )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_photo "${_IMPORT_PREFIX}/lib64/libopencv_photo.so.3.4.1" )
+
+# Import target "opencv_video" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_video APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_video PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_video.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_video.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_video )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_video "${_IMPORT_PREFIX}/lib64/libopencv_video.so.3.4.1" )
+
+# Import target "opencv_dnn" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_dnn APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_dnn PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_dnn.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_dnn.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_dnn )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_dnn "${_IMPORT_PREFIX}/lib64/libopencv_dnn.so.3.4.1" )
+
+# Import target "opencv_imgcodecs" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_imgcodecs APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_imgcodecs PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_imgcodecs.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_imgcodecs.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_imgcodecs )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_imgcodecs "${_IMPORT_PREFIX}/lib64/libopencv_imgcodecs.so.3.4.1" )
+
+# Import target "opencv_shape" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_shape APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_shape PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_shape.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_shape.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_shape )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_shape "${_IMPORT_PREFIX}/lib64/libopencv_shape.so.3.4.1" )
+
+# Import target "opencv_videoio" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_videoio APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_videoio PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_videoio.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_videoio.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_videoio )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_videoio "${_IMPORT_PREFIX}/lib64/libopencv_videoio.so.3.4.1" )
+
+# Import target "opencv_highgui" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_highgui APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_highgui PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_highgui.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_highgui.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_highgui )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_highgui "${_IMPORT_PREFIX}/lib64/libopencv_highgui.so.3.4.1" )
+
+# Import target "opencv_superres" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_superres APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_superres PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_superres.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_superres.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_superres )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_superres "${_IMPORT_PREFIX}/lib64/libopencv_superres.so.3.4.1" )
+
+# Import target "opencv_features2d" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_features2d APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_features2d PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_features2d.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_features2d.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_features2d )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_features2d "${_IMPORT_PREFIX}/lib64/libopencv_features2d.so.3.4.1" )
+
+# Import target "opencv_calib3d" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_calib3d APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_calib3d PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_calib3d.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_calib3d.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_calib3d )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_calib3d "${_IMPORT_PREFIX}/lib64/libopencv_calib3d.so.3.4.1" )
+
+# Import target "opencv_stitching" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_stitching APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_stitching PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_stitching.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_stitching.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_stitching )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_stitching "${_IMPORT_PREFIX}/lib64/libopencv_stitching.so.3.4.1" )
+
+# Import target "opencv_videostab" for configuration "RelWithDebInfo"
+set_property(TARGET opencv_videostab APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(opencv_videostab PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib64/libopencv_videostab.so.3.4.1"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopencv_videostab.so.3.4"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_videostab )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_videostab "${_IMPORT_PREFIX}/lib64/libopencv_videostab.so.3.4.1" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
